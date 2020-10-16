1e7fcb3f0edc757cb7ba44f6baa0d4788ebec2da
Julian zapata rugeles <juliang.@@>
2020-10-16 18:47:34 -0500

    constructor de la clase y metodo get_telemetry

--- gps.py
+++ gps.py
@@ -21,13 +21,54 @@ Objetivo :
         --Generar alertas cuando se encuentre a un radio determinado de coordenadas dadas.
         --Intentar conectarla con una API personal.
 
+Ejemplo de respuesta de la API-TERMUX:
+{
+  "latitude": 5.95905344,
+  "longitude": -75.73410794,
+  "altitude": 1329.3951416015625,
+  "accuracy": 34.30400085449219,
+  "vertical_accuracy": 64.0,
+  "bearing": 188.1999969482422,
+  "speed": 0.699999988079071,
+  "elapsedMs": 4,
+  "provider": "gps"
+}
+
 ###############################################################################################
 """
+#------------------------------------ TEST DATA ---------------------------------------------#
+
+local_test_data="""{
+  "latitude": 6.95912344,
+  "longitude": -73.73235794,
+  "altitude": 1329.3951416015625,
+  "accuracy": 34.30400085449219,
+  "vertical_accuracy": 64.0,
+  "bearing": 188.1999969482422,
+  "speed": 0.699999988079071,
+  "elapsedMs": 4,
+  "provider": "gps"
+}"""
+
+
 import time
 import os
 import json
 
 class GpsTelemtry():
-    gps_information = [];
+    gps_information = {};
 
-    def __init__():
+    def __init__( self, time_elapse , ):
+        self.interval_test = time_elapse
+
+    def get_telemetry(self):
+        try:
+            tmp_raw_data_str = local_test_data # DEBUG: __string__ .
+            #tmp_raw_data_str = os.popen('termux-location -p gps').read()
+            gps_information = json.loads(tmp_raw_data_str)
+        except:
+            print("[Error GPS] : no se obtuvieron coordenadas GPS del dispositivo ")
+        return None
+
+data=GpsTelemtry(1)
+data.get_telemetry()
