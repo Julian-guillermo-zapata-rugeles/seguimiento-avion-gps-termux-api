2660fdfd30270e2c797136cab207c2ae56cc5f3e
saturno <saturno@apolo.org>
2021-05-20 17:33:42 -0500

    se añadió algoritmo de altura y velocidad ms

---
+++ speed.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+print("########### grafica velocidad ############# ")
+file_name = "telemetrias/"+str(input("Nombre archivo sin extencion : "))+".csv"
+try:
+    titulos = ["latitud","longitud","altitud","accuracy","vertical","bearing","speed","elapse","provider","time"]
+    load_dataframe = pd.read_csv(file_name,delimiter=";")
+    load_dataframe.columns = ["latitud","longitud","altitud","accuracy","vertical","bearing","speed","elapse","provider","time"]
+    data_frame_altitud = load_dataframe["speed"].str.replace("speed:","").astype(float)
+    prom = data_frame_altitud[0]
+    counter = 0
+    #data_frame_altitud.plot()
+    for elements in data_frame_altitud:
+        print(elements)
+        prom = ( prom +  elements) // 2
+        data_frame_altitud[counter]=prom
+        counter=counter+1
+
+    print(data_frame_altitud)
+    plt.figure()
+    data_frame_altitud.plot()
+    plt.show()
+
+
+except Exception as e:
+    print("Error al abrir el archivo")
+    print(e)
